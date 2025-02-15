889816bca88d6f90b9f7cb824ad3ad12c6b045d6
octavioserrago <octavioserrago.dev@gmail.com>
2025-02-14 21:59:55 -0300

    Add hero and process components, update routes, and modify link styles

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,22 @@
+<section class="bg-cover bg-crema text-gray-700" style="background-image: url('{{ asset('images/fondo-hero.png') }}'); background-position: 60% 100%;">
+    <div class="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+        <div class="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+            <h1 class="text-5xl font-bold leading-none sm:text-6xl">Vienna Deco
+                <span class="text-rojo">Elegancia y Estilo</span> al alcance de un click ✨
+            </h1>
+            <p class="mt-6 mb-8 text-lg sm:mb-12">
+                En Vienna Deco, creamos muebles y piezas decorativas que elevan tus espacios con estilo y funcionalidad. 
+                Nos especializamos en mesas, mesadas y artículos únicos que combinan mármol premium con bases de hierro 
+                diseñadas a medida, asegurando calidad y elegancia en cada detalle. También ofrecemos tapas de mármol y 
+                accesorios decorativos, junto con soluciones personalizadas para adaptarnos a tus necesidades y estilo. 
+                Ya sea para tu hogar o negocio, nuestras piezas destacan por su diseño exclusivo, durabilidad incomparable 
+                y acabados artesanales. Haz que tu espacio hable de ti con Vienna Deco.
+            </p>
+            <br class="hidden md:inline lg:hidden">
+            <div class="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                <a rel="noopener noreferrer" href="#" class="px-8 py-3 text-lg font-semibold rounded bg-rojo text-white">Ver Catalogo</a>
+                <a rel="noopener noreferrer" href="#" class="px-8 py-3 text-lg font-semibold border rounded border-grisrojo">Consultar por productos personalizados</a>
+            </div>
+        </div>
+    </div>
+</section>
